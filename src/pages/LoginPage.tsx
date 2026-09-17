0abc1b8f3041195ed123f1ef6d3bec14b519449a
zaradigitalseva-commits <zaradigitalseva@gmail.com>
2026-09-17 20:05:33 +0100

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,4 @@
-```tsx
+
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
@@ -395,4 +395,3 @@ export default function LoginPage() {
     </div>
   );
 }
-```
